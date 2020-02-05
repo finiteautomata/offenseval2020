ab366ee75b61f4de81d128e2bd7628f0389b0501
Franco M. Luque <francolq@famaf.unc.edu.ar>
2020-02-04 21:51:11 -0300

    Also download back translations (for data augmentation).

--- bin/get_translations.sh
+++ bin/get_translations.sh
@@ -1,4 +1,7 @@
 wget -nc -O data/translations.tar.gz https://cs.famaf.unc.edu.ar/~francolq/translations.tar.gz
-cd data
-tar -zxvf translations.tar.gz
+wget -nc -O data/back-translations.tar.gz https://cs.famaf.unc.edu.ar/~francolq/back-translations.tar.gz
 
+cd data
+
+tar -zxvf translations.tar.gz
+tar -zxvf back-translations.tar.gz
