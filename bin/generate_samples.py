ab616bebb3f4bd2bd6a7422f941336aae2846790
Juan Manuel Pérez <jmperez.85@gmail.com>
2020-02-05 17:13:30 -0300

    Fix extension

--- bin/generate_samples.py
+++ bin/generate_samples.py
@@ -22,7 +22,7 @@ def generate_samples(sample_frac=0.05, random_state=20202020):
 
     print(f"\nGenerating sample of frac = {sample_frac}")
     base, ext = os.path.splitext(path)
-    sample_path = f"{base}.sample.{ext}"
+    sample_path = f"{base}.sample{ext}"
 
     sample = df_train.sample(frac=sample_frac, random_state=random_state)
     sample.to_csv(sample_path, sep="\t")
