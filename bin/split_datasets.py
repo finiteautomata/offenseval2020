7d13f5606dad4e57585d7ff48d46f1b36a11481d
Juan Manuel Pérez <jmperez.85@gmail.com>
2020-02-11 14:45:22 -0300

    Merge labels for OLID dataset

--- bin/split_datasets.py
+++ bin/split_datasets.py
@@ -9,9 +9,36 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from tqdm import tqdm
 
+def merge_olid():
+    """
+    Merge OLID test set and labels (for subtask A)
+    """
+    print("\n\nMerging OLID test set and labels...")
+
+    df_test = pd.read_table("data/olid/testset-levela.tsv", index_col=0)
+
+    labels_a = pd.read_csv(
+        "data/olid/labels-levela.csv",
+        names=["subtask_a"],
+        index_col=0,
+        header=None
+    )
+
+    df_test = pd.merge(df_test, labels_a, left_index=True, right_index=True)
+
+    assert(all(df_test["subtask_a"].notna()))
+
+    save_path = "data/olid/test_a.tsv"
+    df_test.to_csv(save_path, sep="\t")
+
+    print(f"{df_test.shape[0]} instances saved to {save_path}")
+
+
 def split_datasets(frac=0.2, random_state=20202020):
     """
-    Generate 'dev' splits for datasets
+    Generate 'dev' splits for datasets.
+
+    Also, merge test labels for OLID dataset
     """
 
     files = [
@@ -42,6 +69,8 @@ def split_datasets(frac=0.2, random_state=20202020):
 
         df_dev.to_csv(dev_path, sep="\t")
         print(f"{df_dev.shape[0]} instances saved to {dev_path}")
+
+    merge_olid()
 
 
 if __name__ == "__main__":
