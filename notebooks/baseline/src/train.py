e7ff6061e9054c1cc44619872990d17ecb2d1bf3
Ayme Arango <arangoayme@gmail.com>
2020-02-13 11:15:56 -0300

    train script modified

--- notebooks/baseline/src/train.py
+++ notebooks/baseline/src/train.py
@@ -4,7 +4,9 @@
 from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score, classification_report, confusion_matrix
 import torch.nn as nn
 import torch
-def train(model, current_combination, train_loader, val_loader, device, criterion, optimizer, e, losses, accs):
+from sklearn.metrics import precision_recall_fscore_support
+
+def train(model, current_combination, train_loader, val_loader, device, criterion, optimizer, e, losses, accs, language):
     clip=5 # gradient clipping
 
  
@@ -15,10 +17,12 @@ def train(model, current_combination, train_loader, val_loader, device, criterio
     running_f = 0
 
     for inputs, labels in train_loader:
+
         inputs, labels = inputs.to(device), labels.to(device)
+
         if current_combination['batch_size'] > len(inputs):
             break
-        # print(current_combination['batch_size'])
+
         model.zero_grad()
         output = model(inputs)
         loss = criterion(output.squeeze(), labels.float())
@@ -42,8 +46,14 @@ def train(model, current_combination, train_loader, val_loader, device, criterio
     y_pred = []
     y_true = []
     running_loss = 0
-    running_f = 0
+    running_f = [0,0]
+    running_f_out = 0
+    p = 0
+    r = 0
+    f = 0
 
+    running_precision =  [0,0]
+    running_recall =  [0,0]
     for inputs, labels in val_loader:
 
         inputs, labels = inputs.to(device), labels.to(device)
@@ -54,12 +64,33 @@ def train(model, current_combination, train_loader, val_loader, device, criterio
 
         pred = torch.round(output.squeeze()).cpu().detach().numpy()
         target = labels.float().cpu().detach().numpy()   
+            
+        precision_m, recall_m, fscore_m, support_m = precision_recall_fscore_support(target, pred, average='macro')
+        precision, recall, fscore, support = precision_recall_fscore_support(target, pred, average= None)
 
-        running_f += f1_score(target,pred)
+        p += precision_m
+        r += recall_m
+        f += fscore_m
         running_loss += val_loss.item()
+        
+        running_precision[0] += precision[0]
+        try:
+            running_precision[1] += precision[1]
+        except:
+            pass
+
+        running_recall[0] += recall[0]
+        try:
+            running_recall[1] += recall[1]
+        except:
+            pass
+
+        running_f[0] += fscore[0]
+        try:
+            running_f[1] += fscore[1]
+        except:
+            pass
 
     val_loss = running_loss / len(val_loader)
-    val_f = running_f/ len(val_loader)
-    losses['validation'].append(val_loss)
-    accs['validation'].append(val_f)
-    return val_loss, val_f, losses, accs
+
+    return val_loss, running_precision, running_recall, running_f, p, r, f
