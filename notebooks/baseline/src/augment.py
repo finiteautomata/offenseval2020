5d6811f58e9537e5abb65eda8ebfbc8fa7b3c378
Franco M. Luque <francolq@famaf.unc.edu.ar>
2020-02-20 20:18:44 -0300

    Bugfixes.

--- notebooks/baseline/src/augment.py
+++ notebooks/baseline/src/augment.py
@@ -2,12 +2,6 @@ import pandas as pd
 
 
 def augment_danish(data_train, remove_duplicates=True):
-    # in danish dataset last column is wrong
-    data_train = data_train[:-1]
-    
-    # convert ids to int
-    data_train = data_train.astype({'id': int})
-
     # will be used when calling join
     data_labels = data_train[['id','subtask_a']].set_index('id')
 
@@ -17,13 +11,12 @@ def augment_danish(data_train, remove_duplicates=True):
         augm_data = pd.read_json(fn)
         augm_data = augm_data.rename(columns={'da': 'tweet'})
         augm_data = augm_data.join(data_labels, on='id')
+        augm_data = augm_data.dropna()  # remove data not in data_train
         all_augm_data[lang] = augm_data
     
     all_data = pd.concat([data_train] + list(all_augm_data.values()))
     
     if remove_duplicates:
-        # remove duplicates
-        # (from 14800 to 12773 entries)
         all_data = all_data.drop_duplicates(subset='tweet')
     
     return all_data
