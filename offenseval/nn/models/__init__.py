c9cadd5eb3c1cc485e37e843e7887756ba0fcd47
Juan Manuel Pérez <jmperez.85@gmail.com>
2020-02-20 15:43:43 -0300

    Move models to the module

--- offenseval/nn/models/__init__.py
+++ offenseval/nn/models/__init__.py
@@ -1,2 +1,7 @@
 from .bert_gru import BERTGRUSequenceClassifier
 from .bert_for_sequence import BertSeqModel
+from .CNN_model import CNN
+from .FNN_model import FNN
+from .LSTM_model import LSTM
+from .LSTMATTN_model import LSTMATTN, Attention
+from .MHAttention_model import MHAttention
