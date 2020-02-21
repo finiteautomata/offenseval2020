bb9ce0d17543b501420df66f8c041fcbe53e5a25
Ayme Arango <arangoayme@gmail.com>
2020-02-21 15:56:31 -0300

    augmented arabe

--- notebooks/baseline/src/augment.py
+++ notebooks/baseline/src/augment.py
@@ -20,3 +20,23 @@ def augment_danish(data_train, remove_duplicates=True):
         all_data = all_data.drop_duplicates(subset='tweet')
     
     return all_data
+
+def augment_arabic(data_train, remove_duplicates=True):
+    # will be used when calling join
+    data_labels = data_train[['id','subtask_a']].set_index('id')
+
+    all_augm_data = {}
+    for lang in 'da el en tr'.split():
+        fn = f'../../data/back-translations/Arabic/offenseval-ar-training-v1-{lang}.json'
+        augm_data = pd.read_json(fn)
+        augm_data = augm_data.rename(columns={'ar': 'tweet'})
+        augm_data = augm_data.join(data_labels, on='id')
+        augm_data = augm_data.dropna()  # remove data not in data_train
+        all_augm_data[lang] = augm_data
+    
+    all_data = pd.concat([data_train] + list(all_augm_data.values()))
+    
+    if remove_duplicates:
+        all_data = all_data.drop_duplicates(subset='tweet')
+    
+    return all_data
