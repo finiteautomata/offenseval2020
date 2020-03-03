5efe037394b41c7188eb7008595e6c50af9d2cd9
Juan Manuel Pérez <jmperez.85@gmail.com>
2020-03-03 11:23:26 -0300

    Download test data

--- bin/get_data.sh
+++ bin/get_data.sh
@@ -32,3 +32,30 @@ rm *.zip
 cd olid
 unzip olid.zip
 rm olid.zip
+cd ../..
+
+######################################
+###          TEST DATA             ###
+######################################
+# Run from here to only retrieve test data
+#
+cd data/
+
+download_and_unzip(){
+    wget -nc -O $1/test$3.zip $2
+    cd $1 && unzip -o test$3.zip && rm test$3.zip && cd ..
+}
+
+download_and_unzip Turkish https://competitions.codalab.org/my/datasets/download/10cdc735-ee5e-4ab9-8663-e99b4aae9077
+download_and_unzip Greek https://competitions.codalab.org/my/datasets/download/b6bef21a-87fd-4ea2-8a89-ede45f355c7b
+download_and_unzip Danish https://competitions.codalab.org/my/datasets/download/6bdaf54f-e52b-4bd0-a11c-c8a3ed9dc77d
+download_and_unzip English https://competitions.codalab.org/my/datasets/download/75007c72-09e9-4f4e-ac95-d2c5ff5eda62
+download_and_unzip English https://competitions.codalab.org/my/datasets/download/4264b414-30d3-452f-ad5c-ab4679aa63fd _b
+wget -nc -O Arabic/test.tsv https://competitions.codalab.org/my/datasets/download/9d850022-55e4-4311-be26-50de0094d16a
+
+mv Greek/testset_taska.tsv Greek/test.tsv
+mv Arabic/test.tsv Arabic/test.tsv
+mv Turkish/offenseval-tr-testset-v1.tsv Turkish/test.tsv
+mv Danish/offenseval-da-test-v1-nolabels.tsv Danish/test.tsv
+mv English/test_a_tweets.tsv English/test.tsv
+mv English/test_b_tweets.tsv English/test_b.tsv
