466bc6da9bd4ce532517ae16ed6fb110dd5350fd
Juan Manuel Pérez <jmperez.85@gmail.com>
2020-04-27 17:29:33 -0300

    Add convenient method for report

--- offenseval/nn/report.py
+++ offenseval/nn/report.py
@@ -1,7 +1,19 @@
+import torch
+from sklearn.metrics import accuracy_score, f1_score
+
 class EvaluationReport:
     """
     Class that serves as an EvaluationReport of a model
     """
+    @classmethod
+    def from_preds_and_labels(cls, preds, labels):
+        if type(preds) is torch.Tensor:
+            preds = preds.numpy()
+        pos_f1 = f1_score(labels, preds)
+        neg_f1 = f1_score(1-labels, 1-preds)
+        acc = accuracy_score(labels, preds)
+
+        return cls(acc=acc, pos_f1=pos_f1, neg_f1=neg_f1)
 
     def __init__(self, acc, pos_f1, neg_f1, loss=None):
         self.macro_f1 = (pos_f1 + neg_f1) / 2
@@ -14,7 +26,7 @@ class EvaluationReport:
     def __repr__(self):
         ret = ""
         if self.loss:
-            ret += f'Loss: {self.loss:.3f}'
-        ret += f' Acc: {self.acc*100:.2f}%'
+            ret += f'Loss: {self.loss:.3f} '
+        ret += f'Acc: {self.acc*100:.2f}%'
         ret += f' Macro F1 {self.macro_f1:.3f} (P {self.pos_f1:.3f} - N {self.neg_f1:.3f})'
         return ret
